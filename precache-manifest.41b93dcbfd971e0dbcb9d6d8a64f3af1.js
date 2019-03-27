2f98334e500c533242cb3cbc5d0b8f1a86c430a5
satinder <satinder042890@gmail.com>
2019-03-27 12:05:07 -0700

    Updates

---
+++ precache-manifest.41b93dcbfd971e0dbcb9d6d8a64f3af1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "881d99c882263837c6e5",
+    "url": "/catclickygame/static/js/runtime~main.cba90396.js"
+  },
+  {
+    "revision": "138f0a04b46c13f8f0b9",
+    "url": "/catclickygame/static/js/main.16a54242.chunk.js"
+  },
+  {
+    "revision": "897927156d34efe5fc04",
+    "url": "/catclickygame/static/js/2.927e4b71.chunk.js"
+  },
+  {
+    "revision": "138f0a04b46c13f8f0b9",
+    "url": "/catclickygame/static/css/main.386b6a66.chunk.css"
+  },
+  {
+    "revision": "37287eff2b61840e9269f9751efa24e0",
+    "url": "/catclickygame/index.html"
+  }
+];
